82d905a0d629c6fbd9552f0aed81bed678d356c8
poptaro <noahahn199@gmail.com>
2025-03-29 15:37:49 -0500

    cats image api called correctly

--- pets_project/cat_app/urls.py
+++ pets_project/cat_app/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path("cats/", All_Cat.as_view()),
+    path("cats/image", Cat_Image.as_view()),
 ]
--- pets_project/cat_app/views.py
+++ pets_project/cat_app/views.py
@@ -3,9 +3,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Cat
 from .serializers import CatSerializer
-# Create your views here.
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
 
 class All_Cat(APIView):
   def get(self, request):
     dogs = CatSerializer(Cat.objects.all(), many=True).data
     return Response(dogs)
+  
+class Cat_Image(APIView):
+  def get(self, request):
+    r = requests.get("https://api.thecatapi.com/v1/images/search?api_key={CAT_API_KEY}")
+    print(r.json())
+    return Response(r.json()[0]['url'])
